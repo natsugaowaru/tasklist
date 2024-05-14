31bfdfd17f43993fc2476f097973240eab306c67
nao.mitsuhashi <user094.dounyu@techacademy.jp>
2024-05-14 10:24:36 +0900

    getAllMessagesを追加

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -3,9 +3,9 @@ package utils;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
+//ほとんどの行はおまじないだとのこと
 public class DBUtil {
-    private static final String PERSISTENCE_UNIT_NAME = "tasks";
+    private static final String PERSISTENCE_UNIT_NAME = "tasklist";//多分persistanseの一般のとこにある名前じゃないとエラーになる！！
     private static EntityManagerFactory emf;
 
     public static EntityManager createEntityManager() {
